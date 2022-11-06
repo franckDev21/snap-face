ef7509d74cd8da27d2f9b6bfc96da5ecab42a4b1
franckDev21 <tutodevphp@gmail.com>
2022-11-06 17:47:03 +0100

    update app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,31 +7,40 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  mySnap !: FaceSnap;
-  myOtherSnap !: FaceSnap;
-  myLastSnap !: FaceSnap;
+  faceSnaps !: FaceSnap[];
 
   ngOnInit(){
-    this.mySnap = new FaceSnap(
-      'Franck Tiomela',
-      'Le milliardaire heureux !',
-      new Date(),
-      0,
-      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg'
-    )
-    this.myOtherSnap = {
-      title: 'Three Rock Mountain',
-      description: 'Un endroit magnifique pour les randonnées.',
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
-      createdDate: new Date(),
-      snaps: 0
-    };
-    this.myLastSnap = {
-      title: 'Un bon repas',
-      description: 'Mmmh que c\'est bon !',
-      imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
-      createdDate: new Date(),
-      snaps: 0
-    };
+    this.faceSnaps = [
+      {
+        title : 'Franck Tiomela',
+        description : 'Le milliardaire heureux !',
+        createdDate: new Date(),
+        snaps: 120,
+        imageUrl : 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        location : 'Paris 5'
+      },
+      {
+        title: 'Three Rock Mountain',
+        description: 'Un endroit magnifique pour les randonnées.',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
+        createdDate: new Date(),
+        snaps: 50
+      },
+      {
+        title: 'Un bon repas',
+        description: 'Mmmh que c\'est bon !',
+        imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+        createdDate: new Date(),
+        snaps: 20
+      },
+      {
+        title : 'Franck Tiomela',
+        description : 'Le milliardaire heureux !',
+        createdDate: new Date(),
+        snaps: 3,
+        imageUrl : 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        location : 'Paris 5'
+      }
+    ];
   }
 }
